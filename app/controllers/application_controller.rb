3664f6ce04abf03ba7dc1a13f55f5e5538d43680
cocoared <dmm23program@gmail.com>
2022-05-01 21:51:13 +0000

    [Create]ヘッダー、新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramerters, if: :devise_controller?
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+
+  protected
+
+  def configure_permitted_paramerters
+    devise_paramerter_sanitizer.permit(:sign_up, keys: [:name])
+  # devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録の際に、nameのデータ操作を許可している
+  end
 end
