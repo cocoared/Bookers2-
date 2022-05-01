9ac0dd99f0c4250f804ba033384b465567d03620
cocoared <dmm23program@gmail.com>
2022-05-01 01:11:44 +0000

    [Create]Booksモデル・アソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_235119) do
+ActiveRecord::Schema.define(version: 2022_05_01_004047) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
