6379d347faf708f997cde795c352fac55d3b972e
cocoared <dmm23program@gmail.com>
2022-05-07 12:28:30 +0000

    [Fix]新規登録機能、レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_paramerters, if: :devise_controller?
-  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+# devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def sign_out_path_for(resource)
+    root_path
+  end
 
   protected
 
-  def configure_permitted_paramerters
-    devise_paramerter_sanitizer.permit(:sign_up, keys: [:name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   # devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録の際に、nameのデータ操作を許可している
   end
 end
