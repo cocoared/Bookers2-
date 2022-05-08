81bf61ce05cfbfd76d5ebb9adbd3302a80314b52
cocoared <dmm23program@gmail.com>
2022-05-08 11:26:04 +0000

    [Create]Usersコントローラー、Booksコントローラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,41 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path(@book.id)
+  end
+
   def index
+    @book = Book.all
+    @book = Book.new(book_params)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new(book_params)
+
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,35 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
   def index
+    @users = User.all
+    @user =current_user
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to request.referer
+    flash[:notice] = "ユーザー情報を編集しました。"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
