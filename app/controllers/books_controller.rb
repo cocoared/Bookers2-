92bb9628048152f0379d9b85a3ee0bc77e085b96
cocoared <dmm23program@gmail.com>
2022-05-11 13:14:26 +0000

    [Finish]ユーザー詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,19 +7,20 @@ class BooksController < ApplicationController
   def create
     @user = current_user
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to books_path(@book.id)
+    redirect_to book_path(@book.id)
   end
 
   def index
     @book = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-    @newbook = Book.new(book_params)
-
+    @newbook = Book.new
+    @user = @book.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    
   end
 
   def edit
