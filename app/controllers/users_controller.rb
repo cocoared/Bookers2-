f5b049f7da5e944346eae28d1fcbf43773df300b
cocoared <dmm23program@gmail.com>
2022-05-18 14:48:04 +0000

    [Add]ユーザー編集画面　バリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+      flash[:alert] = "他のユーザーの編集ページにはアクセスできません。"
+    end
   end
 
   def update
