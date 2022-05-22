87a120ac514f1b15a0c8cd6fbc8368b72f256aa6
cocoared <dmm23program@gmail.com>
2022-05-22 11:57:07 +0000

    [Start]書籍いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true
+  has_many :favorite, dependent: :destroy
+  def favorite_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_122815) do
+ActiveRecord::Schema.define(version: 2022_05_22_115131) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2022_05_10_122815) do
     t.string "title", null: false
     t.string "body", null: false
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
