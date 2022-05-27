d1876af2de6690bd6ce0dd8a1f8596f1b242974f
cocoared <dmm23program@gmail.com>
2022-05-27 12:23:11 +0000

    [Add]フォロー機能関連

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destory]
+    get "follows" => "relationships#follows", as: "follows"
+    get "followers" => "relationships#followers", as: "followers"
+  end
+
 
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
